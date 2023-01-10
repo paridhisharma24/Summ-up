99f494f3170cbab48e270585e7ed421b58ecd272
harshilgupta <harshil.gupta.feb8@gmail.com>
2023-01-11 00:00:39 +0530

    working-flask

--- flask_var.py
+++ flask_var.py
@@ -5,14 +5,21 @@ app = Flask(__name__)
 def index():
     return render_template('Minor.html')
 
-@app.route('/ProcessUserinfo/<string:userinfo>',methods=['POST'] )
-def ProcessUserinfo(userinfo):
-    userinfo=json.loads( userinfo)
-    username=userinfo
-    print()
-    print (username)
-    print()
-    return( '/' )
-
+# @app.route('/ProcessUserinfo/<string:userinfo>',methods=['POST'] )
+# def ProcessUserinfo(userinfo):
+#     userinfo=json.loads( userinfo)
+#     username=userinfo
+#     print()
+#     print (username)
+#     print()
+#     return( '/' )
+@app.route('/anything',methods=['POST', 'GET'])
+def anything():
+        text=request.form.get("myValue")
+        return render_template("abc.html", text=text)
+@app.route('/anything1',methods=['POST', 'GET'])
+def anything1():
+        text1=request.form.get("HEYHEY")
+        return render_template("abc.html", text=text1)
 if __name__ == '__main__':
     app.run(debug=True)
